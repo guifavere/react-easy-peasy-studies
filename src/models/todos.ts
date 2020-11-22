83188b3addc9824d6d5847f62ad87734939bd793
guifavere <guilherme.favere@gmail.com>
2020-11-22 15:28:39 -0300

    Studies using typescript

--- src/models/todos.ts
+++ src/models/todos.ts
@@ -1,15 +1,23 @@
-import { Action, action } from 'easy-peasy';
+import { Action, Thunk, action, thunk } from 'easy-peasy';
 
 export interface TodosModel {
   items: string[];
+  asyncAdd: Thunk<TodosModel, string>;
   add: Action<TodosModel, string>;
   delete: Action<TodosModel, string>;
 }
 
 const todos: TodosModel = {
   items: ['todo 1', 'todo 2', 'todo 3'],
+  asyncAdd: thunk(async (actions, payload) => {
+    setTimeout(() => actions.add(payload), 2000);
+  }),
   add: action((state, payload) => {
-    state.items.push(payload);
+    const item = state.items.find(i => i === payload);
+
+    if (!item) {
+      state.items.push(payload);
+    }
   }),
   delete: action((state, payload) => {
     state.items = state.items.filter(item => item !== payload);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useStoreActions, useStoreState } from '../store';
 
-const Home: React.FC = () => {
+export default function Home() {
+  const [asyncOp, setAsyncOp] = useState(false);
+
+  const [todo, setTodo] = useState('');
+
   const todos = useStoreState(state => state.todos.items);
-  const deleteTodo = useStoreActions(actions => actions.todos.delete);
+
+  const { asyncAddTodo, addTodo, deleteTodo } = useStoreActions(actions =>
+    ({
+      asyncAddTodo: actions.todos.asyncAdd,
+      addTodo: actions.todos.add,
+      deleteTodo: actions.todos.delete
+    })
+  );
+
+  const handleChangeAsyncOp = useCallback(
+    () => setAsyncOp(oldAsyncOp => !oldAsyncOp),
+    []
+  );
+
+  const handleChangeTodo = useCallback((event: FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+
+    setTodo(value);
+  }, []);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (asyncOp) {
+      asyncAddTodo(todo);
+    } else {
+      addTodo(todo);
+    }
+
+    setTodo('');
+  }
 
   return (
     <>
       <h1>Home</h1>
+
+      <form onSubmit={handleSubmit}>
+        <p>Create new todo item</p>
+
+        <fieldset>
+          <input onChange={handleChangeAsyncOp} type="checkbox" />
+          <label> async?</label>
+        </fieldset>
+
+        <fieldset>
+          <input onChange={handleChangeTodo} placeholder="name..." type="text" value={todo} />
+          <button title="save" type="submit">save</button>
+        </fieldset>
+      </form>
+
       <ul>
       {todos.map(item => (
         <li key={item}>
@@ -22,5 +71,3 @@ const Home: React.FC = () => {
     </>
   );
 }
-
-export default Home;
